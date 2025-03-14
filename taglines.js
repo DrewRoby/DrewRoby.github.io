be370ae3639f4a040dc1d14e4e235bb86b66d6b2
Roby <drew.roby@cgi.com>
2025-03-14 17:54:42 -0500

    add taglines.js

---
+++ taglines.js
@@ -0,0 +1,24 @@
+// taglines.js
+const taglines = [
+  "digital solutions for the right price",
+  "ashes to ashes",
+  "under construction since 2003",
+  "best viewed at 800x600",
+  "I'm not owned! I'm not owned!",
+  "...but we don't want the Irish!",
+  "error: failed to load MIDI background music",
+  "Old age and treachery will beat youth and skill any day",
+  "everything's computer",
+  "we do not negotiate with terrorists",
+  "optimized for 56k modems",
+  "these colors don't run (how do I remove eggplant stains?)",
+  "[your ad here]",
+  "hah! GAYYYYY!",
+  "there is no way that you are from my loins",
+  "serious inquiries only",
+  "certified Odoo partner",
+  "srs bsns",
+  "one love",
+  "duck tales! ah woo-ooh!",
+  "guitar riffs, flames, confetti, and thou"
+];
